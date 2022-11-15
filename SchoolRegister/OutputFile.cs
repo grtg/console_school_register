5d16d9d5c469a8ece41c85d033682a86f455fb39
Greta <grigaityte.greta@gmail.com>
2022-11-15 18:02:34 +0200

    Repositories and program files added

---
+++ SchoolRegister/OutputFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegister
+{
+    internal class OutputFile
+    {
+    }
+}
