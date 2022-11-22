ba74652fcfbd5c017aaf5c33f85b5da898e04fcb
Greta <grigaityte.greta@gmail.com>
2022-11-22 18:02:58 +0200

    Classes and repositories updated, test added

---
+++ SchoolRegister/Entities/Grades.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolRegister;
+
+namespace SchoolRegister
+{
+    public class Grades
+    {
+        public int ID { get; set; }
+        public int Trimester { get; set; }
+        public int Grade { get; set; }
+        public Grades()
+        {
+
+        }
+        public Grades(int id, int trimester, int grade)
+        {
+            ID = id;
+            Trimester = trimester;
+            Grade = grade;
+        }
+    }
+}
---
+++ SchoolRegisterTest/AddGradeTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SchoolRegisterTest
+{
+    [TestClass]
+    public class AddGradeTest
+    {
+       
+        [TestMethod]
+        public void AddGrade()
+        {
+          
+            var grade = new Grade();
+            grade.AddGrade();
+
+            var result = grade.Add(new Grades(id, trimester, grade));
+            var expected = true;
+
+            Assert.AddGrade(expected, actual);
+
+        }
+    }
+
+ 
+}
